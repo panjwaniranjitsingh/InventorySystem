ffc5e9f38509fbdacbd094af1b69fdcd7873f7f1
Panjwani Ranjit Singh <80108326+panjwaniranjitsingh@users.noreply.github.com>
2021-06-18 22:10:45 +0530

    Created Inventory System

---
+++ Assets/Scripts/ChestManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChestManager : MonoBehaviour
+{
+    [SerializeField] GameObject[] ChestSlots;
+    [SerializeField] ChestScriptableObjectList chestSOL;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void CreateChest()
+    {
+        //Create Chest
+        int randomChest = UnityEngine.Random.Range(0, chestSOL.Chests.Length);
+        ChestSlots[0].transform.GetChild(0).GetComponent<Text>().text = chestSOL.Chests[randomChest].TimeToUnlockInSeconds.ToString();
+        ChestSlots[0].transform.GetChild(1).GetComponent<Text>().text = chestSOL.Chests[randomChest].Type.ToString();
+    }
+}
